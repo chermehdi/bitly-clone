f1e567e0c4d193bad6240df4fb1090686969277f
chermehdi <mehdi.cheracher@gmail.com>
2018-11-28 23:37:48 +0100

    initial backend commit

---
+++ back-end/src/main/java/io/github/chermehdi/bitly/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package io.github.chermehdi.bitly;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint.
+ *
+ * @author chermehdi
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
